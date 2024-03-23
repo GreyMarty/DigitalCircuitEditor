5e1de8a904790d1d8e7b1d94ffd0dce45253ed65
GreyMarty <ikozhan9@gmail.com>
2024-03-23 16:09:27 +0300

    Refactor code

--- Editor.Core/Components/Position.cs
+++ Editor.Core/Components/Position.cs
@@ -1,11 +1,14 @@
 ﻿using System.ComponentModel;
 using System.Numerics;
 using Editor.Component;
+using Editor.Core.Converters;
 
 namespace Editor.Core.Components;
 
 public class Position : ComponentBase<EditorWorld>, INotifyPropertyChanged
 {
+    private IUnitsToPixelsConverter _converter = default!;
+    
     public Vector2 Value { get; set; }
 
     public float X
@@ -19,7 +22,30 @@ public class Position : ComponentBase<EditorWorld>, INotifyPropertyChanged
         get => Value.Y;
         set => Value = Value with { Y = value };
     }
+
+    public Vector2 ValuePixels
+    {
+        get => _converter.ToPixels(Value);
+        set => Value = _converter.ToUnits(value);
+    }
+
+    public float PixelsX
+    {
+        get => ValuePixels.X;
+        set => ValuePixels = ValuePixels with { X = value };
+    }
     
-    
+    public float PixelsY
+    {
+        get => ValuePixels.Y;
+        set => ValuePixels = ValuePixels with { Y = value };
+    }
+        
     public event PropertyChangedEventHandler? PropertyChanged;
+
+
+    public override void Init(EditorWorld world, Entity entity)
+    {
+        _converter = world.PositionConverter;
+    }
 }
--- Editor.Core/ViewModels/BddNodeViewModel.cs
+++ Editor.Core/ViewModels/BddNodeViewModel.cs
@@ -1,50 +1,19 @@
-﻿using System.ComponentModel;
-using System.Numerics;
-using Editor.Component;
+﻿using Editor.Component;
 using Editor.Core.Components;
-using Editor.Core.Converters;
 
 namespace Editor.Core.ViewModels;
 
-public partial class BddNodeViewModel : ViewModelComponentBase
+public class BddNodeViewModel : ViewModelComponentBase
 {
-    private IUnitsToPixelsConverter _positionConverter = default!;
-    private Position _positionComponent = default!;
-    
-    
-    public Vector2 Position => _positionConverter.ToPixels(_positionComponent.Value);
-    public float PositionX => Position.X;
-    public float PositionY => Position.Y;
-    
+    public Position Position { get; private set; } = default!;
+    public Hoverable Hoverable { get; private set; } = default!;
     
     public string? Label { get; set; }
 
-    public Hoverable Hoverable { get; private set; } = new();
 
     public override void Init(EditorWorld world, Entity entity)
     {
-        _positionConverter = world.PositionConverter;
-        
-        _positionComponent = entity.GetRequiredComponent<Position>();
-        _positionComponent.PropertyChanged += PositionComponent_OnPropertyChanged;
-
+        Position = entity.GetRequiredComponent<Position>();
         Hoverable = entity.GetRequiredComponent<Hoverable>();
-    }
-
-    public override void Dispose()
-    {
-        _positionComponent.PropertyChanged -= PositionComponent_OnPropertyChanged;
-    }
-    
-    private void PositionComponent_OnPropertyChanged(object? sender, PropertyChangedEventArgs e)
-    {
-        switch (e.PropertyName)
-        {
-            case nameof(_positionComponent.Value):
-                OnPropertyChanged(nameof(Position));
-                OnPropertyChanged(nameof(PositionX));
-                OnPropertyChanged(nameof(PositionY));
-                break;
-        }
     }
 }
