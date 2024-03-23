74574c83c6c1efee4cfcbbedebb5651d874e6bb2
GreyMarty <ikozhan9@gmail.com>
2024-03-23 17:09:14 +0300

    Add Rebind helper method

---
+++ Editor.Core/ViewModels/PropertyChanged.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace Editor.Core.ViewModels;
+
+public static class PropertyChanged
+{
+    public static T? Rebind<T>(this T? from, T? to, PropertyChangedEventHandler propertyChanged)
+        where T : INotifyPropertyChanged
+    {
+        if (from is not null)
+        {
+            from.PropertyChanged -= propertyChanged;
+        }
+
+        if (to is not null)
+        {
+            to.PropertyChanged += propertyChanged;
+        }
+
+        return to;
+    }
+}
