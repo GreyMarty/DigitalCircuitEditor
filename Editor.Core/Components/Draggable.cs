0a11e67dce9249942bf904555fd4e80857e70b22
GreyMarty <ikozhan9@gmail.com>
2024-03-23 12:14:24 +0300

    Add Draggable component

---
+++ Editor.Core/Components/Draggable.cs
@@ -0,0 +1,31 @@
+﻿using Editor.Component;
+using Editor.Core.Events;
+using Editor.Core.Input;
+
+namespace Editor.Core.Components;
+
+public class Draggable : ComponentBase<EditorWorld>
+{
+    private Position _positionComponent = default!;
+    private Hoverable _hoverableComponent = default!;
+    
+    
+    public override void Init(EditorWorld world, Entity entity)
+    {
+        _positionComponent = entity.GetRequiredComponent<Position>();
+        _hoverableComponent = entity.GetRequiredComponent<Hoverable>();
+
+        world.EventBus.Subscribe<MouseMove>(OnMouseMove);
+    }
+    
+    private void OnMouseMove(MouseMove e)
+    {
+        if (!_hoverableComponent.Hovered || e.Button != MouseButton.Left!)
+        {
+            return;
+        }
+        
+        var delta = e.NewPosition - e.OldPosition;
+        _positionComponent.Value += delta;
+    }
+}
