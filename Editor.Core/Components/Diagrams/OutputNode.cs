e5f6d6c4a1f3afdb96b029fab1321beb12ca120d
GreyMarty <ikozhan9@gmail.com>
2024-04-11 15:41:53 +0300

    Fix output nodes ids

--- Editor.Core/Components/Diagrams/OutputNode.cs
+++ Editor.Core/Components/Diagrams/OutputNode.cs
@@ -2,11 +2,29 @@
 
 public class OutputNode<TConnectionType> : BranchNode<TConnectionType> where TConnectionType : notnull
 {
-    public int OutputId { get; set; }
+    public int OutputId { get; private set; }
 
     public override string? Label
     {
         get => $"F{OutputId}";
         set { }
+    }
+
+
+    protected override void OnInit()
+    {
+        base.OnInit();
+
+        var ids = Context.Entities
+            .Where(x => x != Entity)
+            .Select(x => x.GetComponent<OutputNode<TConnectionType>>()?.Component)
+            .Where(x => x is not null)
+            .Select(x => x.OutputId)
+            .ToList();
+        
+        if (ids.Count > 0)
+        {
+            OutputId = ids.Max() + 1;
+        }
     }
 }
